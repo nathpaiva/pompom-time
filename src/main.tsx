1124908e87b4a361d1db96ce0e348a0d48f3a9d0
Nath Paiva <hello@nathpaiva.com.br>
2023-09-11 12:49:06 -0700

    feat: create navigation and route

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 
-import { App } from './App'
+import { router } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>,
 )
