96a09a06eeafe9d6c6d77aaab71ecbe3a0659595
Nath Paiva <hello@nathpaiva.com.br>
2023-09-12 14:32:53 -0700

    feat: updates App tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import { describe, expect, it } from 'vitest'
 import { App } from './App'
 
 describe('App', () => {
-  it('should render the app with two buttons', () => {
+  it('should render the app with navigation and the user logged out', () => {
     const { getByText } = render(<App />)
     expect(getByText('logout')).not.toBeVisible()
     expect(getByText('login')).toBeVisible()
