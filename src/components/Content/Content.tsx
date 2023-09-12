81d94ece9ddac60ab0ca7e86fc19afc05f99ffcb
Nath Paiva <hello@nathpaiva.com.br>
2023-09-12 12:33:00 -0700

    feat: removes log

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -6,8 +6,6 @@ import { useAuth } from '../Auth'
 export const Content = () => {
   const { user } = useAuth()
 
-  console.log('user', user)
-
   return (
     <GridItem>
       {user && <h1>Hello, {user.user_metadata?.full_name}</h1>}
