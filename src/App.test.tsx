70aa8c1ed8366a25b4a9f53a850cf89a41b9647d
Nath Paiva <hello@nathpaiva.com.br>
2023-09-14 14:47:44 -0700

    fix: changes App test to cover auth and not auth

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,11 +2,41 @@ import { render } from '@utils/test'
 
 import { App } from './App'
 
+const { _useIdentityContext } = vi.hoisted(() => {
+  return { _useIdentityContext: vi.fn() }
+})
+
+vi.mock('react-netlify-identity', () => ({
+  useIdentityContext: _useIdentityContext,
+}))
+
 describe('App', () => {
-  it('should render the app with navigation and the user logged out', () => {
-    const { getByText } = render(<App />)
-    expect(getByText('logout')).not.toBeVisible()
-    expect(getByText('login')).toBeVisible()
-    expect(getByText('workout time')).toBeVisible()
+  beforeEach(() => {
+    vi.resetAllMocks()
+  })
+
+  describe('user logged out', () => {
+    it('should render not auth navigation', () => {
+      vi.mocked(_useIdentityContext).mockReturnValue({
+        isLoggedIn: false,
+      })
+
+      const { getByText } = render(<App />)
+      expect(getByText('logout')).not.toBeVisible()
+      expect(getByText('workout time')).not.toBeVisible()
+      expect(getByText('login')).toBeVisible()
+    })
+  })
+
+  describe('user logged in', () => {
+    it('should render auth navigation', () => {
+      vi.mocked(_useIdentityContext).mockReturnValue({
+        isLoggedIn: true,
+      })
+      const { getByText } = render(<App />)
+      expect(getByText('logout')).toBeVisible()
+      expect(getByText('workout time')).toBeVisible()
+      expect(getByText('login')).not.toBeVisible()
+    })
   })
 })
--- src/utils/testWrapper.tsx
+++ src/utils/testWrapper.tsx
@@ -1,12 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { render, RenderOptions } from '@testing-library/react'
-import netlifyIdentity from 'netlify-identity-widget'
 import React, { ReactElement } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-
-netlifyIdentity.init({
-  logo: false,
-})
 
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
   return (
