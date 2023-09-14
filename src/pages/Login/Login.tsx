2a281921612963a7a80954077369438431dae11f
Nath Paiva <hello@nathpaiva.com.br>
2023-09-13 17:17:32 -0700

    fix: changes form to register and login with same function

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -5,7 +5,6 @@ import {
   FormControl,
   FormLabel,
   Input,
-  Text,
 } from '@chakra-ui/react'
 import { FormEvent, useCallback, useEffect, useRef, useState } from 'react'
 import { useIdentityContext } from 'react-netlify-identity'
@@ -21,6 +20,68 @@ const buttonStyle = {
   bottom: 0,
 }
 
+const FormComponent = ({
+  onChangeHandle,
+  errorMessage,
+  formType,
+}: {
+  onChangeHandle: (e: FormEvent<HTMLInputElement>) => void
+  errorMessage?: string
+  formType: TFormType
+}) => (
+  <div>
+    {formType === 'register' && (
+      <FormControl as="fieldset" rowGap="2" display="grid">
+        <FormLabel>Full name</FormLabel>
+        <Input
+          onChange={onChangeHandle}
+          type="name"
+          name="fullName"
+          placeholder="name"
+        />
+      </FormControl>
+    )}
+
+    <FormControl as="fieldset" rowGap="2" display="grid">
+      <FormLabel>Email</FormLabel>
+      <Input
+        onChange={onChangeHandle}
+        type="email"
+        name="email"
+        placeholder="Email"
+      />
+    </FormControl>
+
+    <FormControl as="fieldset" rowGap="2" display="grid">
+      <FormLabel>Password</FormLabel>
+      <Input
+        onChange={onChangeHandle}
+        type="password"
+        name="password"
+        placeholder="Password"
+      />
+    </FormControl>
+    {/* if is login should show recover pass button */}
+    {formType === 'login' && (
+      <Button
+        onClick={() => {
+          console.log('>>>>')
+        }}
+        isDisabled
+        sx={{
+          display: 'block',
+          margin: '10px auto',
+        }}
+      >
+        Forgot Password
+      </Button>
+    )}
+    <div>{errorMessage}</div>
+  </div>
+)
+
+type TFormType = 'login' | 'register'
+
 export const Login = () => {
   /**
    * create container ref to have the div width
@@ -28,40 +89,49 @@ export const Login = () => {
    */
   const containerRef = useRef<HTMLDivElement>(null)
   // state to manage the form should be visible
-  const [formFocus, setFormFocus] = useState<'login' | 'register'>('register')
+  const [formFocus, setFormFocus] = useState<TFormType>('login')
   // state to manage the inputs
   const [loginFormData, setFormData] = useState({
     email: undefined,
     password: undefined,
+    fullName: undefined,
   })
-  const { loginUser, isLoggedIn } = useIdentityContext()
+  const { loginUser, isLoggedIn, signupUser } = useIdentityContext()
 
   const [errorMessage, setErrorMessage] = useState<string>()
 
-  const onSubmitHandle = (e: FormEvent<HTMLFormElement>) => {
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    console.log('formFocus', formFocus)
-    async function login() {
-      const { email, password } = loginFormData
-      console.log('>>>>')
-      try {
-        console.log('🚀 ~ file: Login.tsx:48 ~ Login ~ password:', {
-          password,
-          email,
-        })
+    const { id } = e.target as HTMLFormElement & { id: TFormType }
 
+    if (formFocus !== id) {
+      switchForm()
+      return
+    }
+
+    async function _submit() {
+      const { email, password, fullName } = loginFormData
+
+      try {
         if (!email || !password) {
           throw new Error('Email and password are required')
         }
 
-        const user = await loginUser(email, password)
-        console.log('🚀 ~ file: Login.tsx:60 ~ onSubmit={async ~ user:', user)
+        const actionToSubmit =
+          id === 'login'
+            ? async () => await loginUser(email, password)
+            : async () =>
+                await signupUser(email, password, {
+                  full_name: fullName,
+                })
+
+        await actionToSubmit()
       } catch (err) {
         setErrorMessage('Error: ' + (err as Error).message)
       }
     }
 
-    login()
+    _submit()
   }
 
   const onChangeHandle = (e: FormEvent<HTMLInputElement>) => {
@@ -72,11 +142,12 @@ export const Login = () => {
     }))
   }
 
-  const switchForm = (form: 'login' | 'register') => {
-    console.log('veio')
-    if (form === formFocus) return
+  const switchForm = () => {
+    setFormFocus((prevState) => {
+      const switchTo = prevState === 'login' ? 'register' : 'login'
 
-    setFormFocus(form)
+      return switchTo
+    })
   }
 
   const changeStyle = useCallback(
@@ -116,16 +187,23 @@ export const Login = () => {
     >
       {/* register form control */}
       <Box
+        as="form"
+        onSubmit={onSubmit}
         sx={{
           bgColor: 'red.100',
           minHeight: '500px',
           minWidth: `calc(100% - ${IMAGE_CONTAINER_WIDTH_SIZE_PX})`,
           transition: 'transform 250ms',
         }}
+        id="register"
         data-move
       >
         <Greet />
-        <Text>Register</Text>
+        <FormComponent
+          errorMessage={errorMessage}
+          onChangeHandle={onChangeHandle}
+          formType="register"
+        />
       </Box>
 
       {/* nice image */}
@@ -143,7 +221,7 @@ export const Login = () => {
       {/* login form control */}
       <Box
         as="form"
-        onSubmit={onSubmitHandle}
+        onSubmit={onSubmit}
         rowGap="2"
         sx={{
           bgColor: 'orange.100',
@@ -151,59 +229,24 @@ export const Login = () => {
           minWidth: `calc(100% - ${IMAGE_CONTAINER_WIDTH_SIZE_PX})`,
           transition: 'transform 250ms',
         }}
-        id="login-form"
+        id="login"
         data-move
       >
         <Greet />
-        <div>
-          <FormControl as="fieldset" rowGap="2" display="grid">
-            <FormLabel>Email</FormLabel>
-            <Input
-              onChange={onChangeHandle}
-              type="email"
-              name="email"
-              placeholder="Email"
-            />
-          </FormControl>
-
-          <FormControl as="fieldset" rowGap="2" display="grid">
-            <FormLabel>Password</FormLabel>
-            <Input
-              onChange={onChangeHandle}
-              type="password"
-              name="password"
-              placeholder="Password"
-            />
-          </FormControl>
-          <div>{errorMessage}</div>
-        </div>
+        <FormComponent
+          errorMessage={errorMessage}
+          onChangeHandle={onChangeHandle}
+          formType="login"
+        />
       </Box>
 
       {/* actions button */}
-      <Button
-        sx={{ ...buttonStyle, left: 0 }}
-        onClick={() => switchForm('register')}
-      >
+      <Button sx={{ ...buttonStyle, left: 0 }} form="register" type="submit">
         Register
       </Button>
 
       {/* TODO: disabled button when form is submitted */}
-      <Button
-        sx={{ ...buttonStyle, right: 0 }}
-        onClick={
-          formFocus !== 'login'
-            ? () => {
-                switchForm('login')
-              }
-            : undefined
-        }
-        type={formFocus === 'login' ? 'submit' : 'button'}
-        form={formFocus === 'login' ? 'login-form' : undefined}
-        isDisabled={
-          formFocus === 'login' &&
-          Object.values(loginFormData).includes(undefined)
-        }
-      >
+      <Button sx={{ ...buttonStyle, right: 0 }} type="submit" form="login">
         Login
       </Button>
     </Box>
