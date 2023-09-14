2114629a554a361f660ea58753754771d37ac91d
Nath Paiva <hello@nathpaiva.com.br>
2023-09-14 14:51:02 -0700

    fix: changes ProtectedRoute test

--- src/components/ProtectedRoute/ProtectedRoute.test.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.test.tsx
@@ -2,11 +2,35 @@ import { render } from '@utils/test'
 
 import { ProtectedRoute } from './ProtectedRoute'
 
+const { _useIdentityContext } = vi.hoisted(() => {
+  return { _useIdentityContext: vi.fn() }
+})
+
+vi.mock('react-netlify-identity', () => ({
+  useIdentityContext: _useIdentityContext,
+}))
+
 describe('ProtectedRoute', () => {
+  beforeEach(() => {
+    vi.resetAllMocks()
+  })
+
+  it('should test if the user is logged in', () => {
+    vi.mocked(_useIdentityContext).mockReturnValue({
+      isLoggedIn: true,
+    })
+
+    render(<ProtectedRoute />)
+
+    expect(window.location.pathname).toBe('/')
+  })
+
   it('should render redirect to login page', () => {
+    vi.mocked(_useIdentityContext).mockReturnValue({
+      isLoggedIn: false,
+    })
     render(<ProtectedRoute />)
 
     expect(window.location.pathname).toBe('/login')
   })
-  it.todo('should test if the user is logged in')
 })
