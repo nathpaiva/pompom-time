79fb0dde016fe1cda4a5a09f41ff1d5059f4983b
Nath Paiva <hello@nathpaiva.com.br>
2023-09-19 12:24:05 -0700

    chore: moves src to client folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig, type UserConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin, type UserConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import type { InlineConfig } from 'vitest'
 
@@ -7,44 +7,50 @@ interface VitestConfigExport extends UserConfig {
   test: InlineConfig
 }
 
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-    alias: {
-      '@utils/test': new URL('./src/utils/testWrapper.tsx', import.meta.url)
-        .pathname,
-    },
-    coverage: {
-      provider: 'v8',
-      include: ['src/**/*'],
-      reporter: ['text', 'json', 'html'],
-      exclude: [
-        'src/colorPalette.ts',
-        'src/utils/testWrapper.ts',
-        'src/utils/noop.ts',
-        // TODO: after create the theme, remove this line
-        'src/utils/theme.ts',
-        'src/**/index.ts',
-        'src/**/index.tsx',
-        'src/**/*.d.ts',
-        'src/**/mock.ts',
-        'src/**/main.tsx',
-      ],
-      all: true,
-      branches: 40,
-      functions: 50,
-      lines: 50,
-      statements: 50,
-      cleanOnRerun: false,
-    },
+const testConfig = {
+  globals: true,
+  environment: 'jsdom',
+  setupFiles: ['./client/src/setupTests.ts'],
+  alias: {
+    '@utils/test': new URL(
+      './client/src/utils/testWrapper.tsx',
+      import.meta.url,
+    ).pathname,
   },
-  plugins: [react(), tsconfigPaths()],
+  coverage: {
+    provider: 'v8',
+    include: ['client/src/**/*'],
+    reporter: ['text', 'json', 'html'],
+    exclude: [
+      'client/src/colorPalette.ts',
+      'client/src/utils/testWrapper.ts',
+      'client/src/utils/noop.ts',
+      // TODO: after create the theme, remove this line
+      'client/src/utils/theme.ts',
+      'client/src/**/index.ts',
+      'client/src/**/index.tsx',
+      'client/src/**/*.d.ts',
+      'client/src/**/mock.ts',
+      'client/src/**/main.tsx',
+    ],
+    all: true,
+    branches: 40,
+    functions: 50,
+    lines: 50,
+    statements: 50,
+    cleanOnRerun: false,
+  },
+} satisfies VitestConfigExport['test']
+
+const viteConfig = {
+  test: testConfig,
+  plugins: [splitVendorChunkPlugin(), react(), tsconfigPaths()],
   server: {
     port: 4200,
     watch: {
       usePolling: true,
     },
   },
-} as VitestConfigExport)
+} satisfies VitestConfigExport
+
+export default defineConfig(viteConfig)
