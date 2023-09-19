f6ce11cd1a559a6c39b806a801e163183711fbc9
Nath Paiva <hello@nathpaiva.com.br>
2023-09-19 13:10:39 -0700

    chore: fixes lint config

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { IdentityContextProvider } from 'react-netlify-identity'
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from './routes'
 import { customVariant } from './utils'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <IdentityContextProvider url={import.meta.env.VITE_IDENTITY_URL}>
       <ChakraProvider theme={customVariant}>
         <RouterProvider router={router} />
       </ChakraProvider>
     </IdentityContextProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 )
