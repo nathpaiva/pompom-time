b3d01b8eccf1b1a045d7dd071f33309ab6ca1f77
Nath Paiva <hello@nathpaiva.com.br>
2023-09-20 11:46:37 -0700

    chore: changes coverage config

--- serverless/vite.config.ts
+++ serverless/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, type UserConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import type { InlineConfig } from 'vitest'
 import { configDefaults } from 'vitest/config'
 
@@ -7,33 +6,24 @@ interface VitestConfigExport extends UserConfig {
   test: InlineConfig
 }
 
-const testConfig = {
-  globals: true,
-  clearMocks: true,
-  setupFiles: ['dotenv/config'],
-  environment: 'node',
-  exclude: [...configDefaults.exclude, 'client'],
-  coverage: {
-    provider: 'v8',
-    include: ['serverless/**/*'],
-    reporter: ['text', 'json', 'html'],
-    exclude: ['serverless/generated', 'serverless/**/*.generated.ts'],
-    all: true,
-    branches: 40,
-    functions: 50,
-    lines: 50,
-    statements: 50,
-    cleanOnRerun: false,
-  },
-} satisfies VitestConfigExport['test']
-
 const viteConfig = {
-  test: testConfig,
-  plugins: [tsconfigPaths()],
-  server: {
-    port: 4200,
-    watch: {
-      usePolling: true,
+  test: {
+    globals: true,
+    clearMocks: true,
+    setupFiles: ['dotenv/config'],
+    environment: 'node',
+    exclude: [...configDefaults.exclude, 'client'],
+    coverage: {
+      provider: 'v8',
+      include: ['serverless/**/*'],
+      reporter: ['text', 'json', 'html'],
+      exclude: ['serverless/generated', 'serverless/**/*.generated.ts'],
+      all: true,
+      branches: 30, // 40
+      functions: 40, // 50
+      lines: 50,
+      statements: 50,
+      cleanOnRerun: false,
     },
   },
 } satisfies VitestConfigExport
