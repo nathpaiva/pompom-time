680ad27e5b778cd894fef47c5f923b8b19e96cc4
Nath Paiva <hello@nathpaiva.com.br>
2023-09-28 15:37:21 -0700

    chore(serverless): creates the schema for body validation

---
+++ serverless/functions/add-workout-by-user/bodySchema.ts
@@ -0,0 +1,49 @@
+export const bodySchema = {
+  type: 'object',
+  properties: {
+    body: {
+      type: 'object',
+      properties: {
+        repeat: { type: 'boolean' },
+        name: { type: 'string' },
+        goal_per_day: { type: 'integer' },
+        rest: { type: 'integer' },
+        squeeze: { type: 'integer' },
+      },
+      if: {
+        properties: {
+          type: {
+            type: 'string',
+            pattern: '^resistance$',
+          },
+          interval: { type: 'integer' },
+        },
+      },
+      then: {
+        required: ['interval'],
+        errorMessage: {
+          required: 'Interval is required for ${/body/type} workout type.',
+          _: 'You should provide the workout data',
+        },
+      },
+      else: {
+        properties: {
+          type: {
+            type: 'string',
+            pattern: '(^pulse$)|(^strength$)|(^intensity$)',
+          },
+          interval: false,
+        },
+        errorMessage: {
+          properties: {
+            interval: 'Interval is not valid for ${/body/type} workout type.',
+          },
+        },
+      },
+      required: ['name', 'type', 'repeat', 'goal_per_day', 'rest', 'squeeze'],
+    },
+  },
+  errorMessage: {
+    _: 'You should provide the workout data',
+  },
+}
