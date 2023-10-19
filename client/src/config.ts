e97615437829831d8f276947a74a8156a4865bf3
Nath Paiva <hello@nathpaiva.com.br>
2023-10-18 17:03:28 -0700

    fix(client): updates queryClient config and test setup to use sabe config, and remove fetch api from setup

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,3 +1,13 @@
 import { QueryClient } from '@tanstack/react-query'
 
-export const queryClient = new QueryClient()
+/**
+ * set queries retry to false to test easily errors
+ * we can change it to be dynamically
+ */
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: process.env.NODE_ENV === 'test' ? false : true,
+    },
+  },
+})
